ec27a5cd3c24d3e6b37d365b7ecb47042e5978ce
Yogesh Pathe <98703537+yugyogeshpawar@users.noreply.github.com>
2023-08-09 11:43:00 +0530

    Update AccountChangePassword.js

--- src/components/_dashboard/user/account/AccountChangePassword.js
+++ src/components/_dashboard/user/account/AccountChangePassword.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import { useSnackbar } from 'notistack5';
 import { useFormik, Form, FormikProvider } from 'formik';
 // material
-import { Stack, Card, TextField } from '@material-ui/core';
+import { Stack, Card, TextField, Typography, Box } from '@material-ui/core';
 import { LoadingButton } from '@material-ui/lab';
 
 // utils
@@ -10,6 +10,7 @@ import { LoadingButton } from '@material-ui/lab';
 // import { changePassword } from '../../../../redux/slices/user';
 // import { useDispatch, useSelector } from '../../../../redux/store';
 import useAuth from '../../../../hooks/useAuth';
+
 // ----------------------------------------------------------------------
 
 export default function AccountChangePassword() {
@@ -47,46 +48,51 @@ export default function AccountChangePassword() {
   const { errors, touched, isSubmitting, handleSubmit, getFieldProps } = formik;
 
   return (
-    <Card sx={{ p: 3 }}>
-      <FormikProvider value={formik}>
-        <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-          <Stack spacing={3} alignItems="flex-end">
-            <TextField
-              {...getFieldProps('oldPassword')}
-              fullWidth
-              autoComplete="on"
-              type="text"
-              label="Old Password"
-              error={Boolean(touched.oldPassword && errors.oldPassword)}
-              helperText={touched.oldPassword && errors.oldPassword}
-            />
+    <Box width="40vw" marginLeft="3rem">
+      <Card sx={{ p: 3 }}>
+        <Typography variant="h4" textAlign="center" mb="1rem">
+          Change Password
+        </Typography>
+        <FormikProvider value={formik}>
+          <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <Stack spacing={3} alignItems="flex-end">
+              <TextField
+                {...getFieldProps('oldPassword')}
+                fullWidth
+                autoComplete="on"
+                type="text"
+                label="Old Password"
+                error={Boolean(touched.oldPassword && errors.oldPassword)}
+                helperText={touched.oldPassword && errors.oldPassword}
+              />
 
-            <TextField
-              {...getFieldProps('newPassword')}
-              fullWidth
-              autoComplete="on"
-              type="text"
-              label="New Password"
-              error={Boolean(touched.newPassword && errors.newPassword)}
-              helperText={(touched.newPassword && errors.newPassword) || 'Password must be minimum 6+'}
-            />
+              <TextField
+                {...getFieldProps('newPassword')}
+                fullWidth
+                autoComplete="on"
+                type="text"
+                label="New Password"
+                error={Boolean(touched.newPassword && errors.newPassword)}
+                helperText={(touched.newPassword && errors.newPassword) || 'Password must be minimum 6+'}
+              />
 
-            <TextField
-              {...getFieldProps('verifyPassword')}
-              fullWidth
-              autoComplete="on"
-              type="text"
-              label="Confirm New Password"
-              error={Boolean(touched.verifyPassword && errors.verifyPassword)}
-              helperText={touched.verifyPassword && errors.verifyPassword}
-            />
+              <TextField
+                {...getFieldProps('verifyPassword')}
+                fullWidth
+                autoComplete="on"
+                type="text"
+                label="Confirm New Password"
+                error={Boolean(touched.verifyPassword && errors.verifyPassword)}
+                helperText={touched.verifyPassword && errors.verifyPassword}
+              />
 
-            <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-              Save Changes
-            </LoadingButton>
-          </Stack>
-        </Form>
-      </FormikProvider>
-    </Card>
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                Save Changes
+              </LoadingButton>
+            </Stack>
+          </Form>
+        </FormikProvider>
+      </Card>
+    </Box>
   );
 }
